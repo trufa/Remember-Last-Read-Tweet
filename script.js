2a2629c82d9e03b0822838eaa0a96b36a4f1188e
Trufa <trufavarela@gmail.com>
2012-12-11 00:52:06 -0200

    checkURL now calls agregarUltimo to minimize calls

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(window).load(function() {
+
 	function checkURL() {
 		
 		if(location.href !== "https://twitter.com/"){
@@ -7,12 +8,24 @@ $(window).load(function() {
 			
 		}else{
 			$('#buscaultimo').show();
+			agregarUltimo();
 		}
 		
 		setTimeout(function() {
 			    checkURL();
 		}, 1000);
 	}
+
+	function agregarUltimo() {
+
+		$('#stream-items-id > div').each(function() {
+			if($(this).find('.savetwlink').text()===""){
+				$(this).find('.tweet-actions').append('<li class="action-fav-container"><a class="savetwlink" rel="' + $(this).attr("id") + '"> Último</a></li>');//&#8c593;
+ 			}		
+		});
+		
+	}
+	
 	checkURL();
 	
 	$('#global-actions').append('<li><a class="js-hover js-nav" id="buscaultimo"><img id="down-arrow"/><p id="texto-buscar">Buscar Último</p></a></li>');
@@ -31,22 +44,14 @@ $(window).load(function() {
 		}
 	});
 	
-	
-
-	function agregarUltimo() {
-
+	function getLastID(){
+		var last = "";
 		$('#stream-items-id > div').each(function() {
-			if($(this).find('.savetwlink').text()===""){
-				$(this).find('.tweet-actions').append('<li class="action-fav-container"><a class="savetwlink" rel="' + $(this).attr("id") + '"> Último</a></li>');//&#8c593;
- 			}  		
+				last = $(this).attr("id");
 		});
-		
-		setTimeout(function() {
-			    agregarUltimo();
-		}, 1000);
+		return last;
 	}
-	agregarUltimo();
-
+	
 	$(document).on('click', '.savetwlink', function()
 	{
 		if(localStorage["lastSeenTw"] != null){
@@ -66,24 +71,24 @@ $(window).load(function() {
 		return true;
 	}
 	
-	//var isCanceled = false;
 
 	function search_tweet(twid) {
 		if(!found_tweet(twid)){
-			$("html, body").animate({ scrollTop: $(document).height() }, 1000);
+			$("html, body").animate({ scrollTop: $(document).height() }, 0);
 			setTimeout(function() {
 			    search_tweet(twid);
 			}, 1000);
 		}else{
-			setTimeout(function(){
+				setTimeout(function(){
 				//window.location.hash = '#' + twid;
 				document.getElementById(twid).scrollIntoView();
 				var y = $(window).scrollTop();
 				$(window).scrollTop(y-220);
 				$('#' +twid).css("border-top","5px solid red");
-  			}, 500);	
+  			}, 500);
 		}
 	}
+
 	
 	
 	//search_tweet();
